991a28c5661f5fd2ea988b6b02216ebf7d117c03
Josue Arronis <cruzjos94@gmail.com>
2021-06-25 06:53:36 -0600

    Example 1

---
+++ Exmple 1/src/store/actions/item.actions.js
@@ -0,0 +1,6 @@
+export const AddItem = (item) => {
+  return {
+    type: 'ADD_ITEM',
+    payload: item,
+  };
+};
---
+++ Exmple 1/src/vue/main.vue
@@ -0,0 +1,49 @@
+
+<script>
+import { connect } from "redux-connect-vue";
+
+const state = (state) => ({
+  Items: state.ItemReducer.items,
+});
+const actions = (dispatch) => ({});
+export default connect(state, actions)({});
+</script>
+
+<template>
+  <div class="form-container">
+    <h1>Vue <span class="text-primary">List</span></h1>
+    <label v-if="Items.length === 0">List Empty please add an Item</label>
+    <div v-if="Items.length > 0">
+     
+    <div class='card bg-light' v-for="(item, index) in Items" :key="index">
+      <h3 class='text-primary text-left'>
+       Name: {{item.name}}
+      </h3>
+      <ul class='list'>
+          <li>
+            <i class='fas fa-envelope-open'></i>Brand: {{item.brand}}
+          </li>
+          <li>
+            <i class='fas fa-phone'></i> Price: {{item.price}}
+          </li>
+      </ul>
+      <p>
+        <button class='btn btn-dark btn-sm'>
+          Edit
+        </button>
+        <button class='btn btn-danger btn-sm'>
+          Delete
+        </button>
+      </p>
+    </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+
+
+
