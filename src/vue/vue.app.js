b9a67d72c45ba8f4941c52c5ed4c7e19a71f659a
Josue Arronis <cruzjos94@gmail.com>
2021-06-24 19:41:30 -0600

    Redux on vue app

--- src/vue/vue.app.js
+++ src/vue/vue.app.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import singleSpaVue from 'single-spa-vue';
+import ReduxConnectVue from 'redux-connect-vue';
+import store from '../store/store';
 import Hello from './main.vue';
 import '../../assets/css/App.css';
+
+Vue.use(ReduxConnectVue, { store });
+
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
